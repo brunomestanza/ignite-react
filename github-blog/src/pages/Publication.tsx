531f7002117eb0313c748c03d9a57b773d912fca
Bruno Mestanza <brunomonteiromestanza@gmail.com>
2022-11-17 11:55:54 -0300

    Finished project

--- github-blog/src/pages/Publication.tsx
+++ github-blog/src/pages/Publication.tsx
@@ -1,5 +1,7 @@
 import { useCallback, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import { formatDistanceToNow } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
 import {
   FaLessThan,
   FaExternalLinkAlt,
@@ -31,6 +33,7 @@ export function Publication() {
         `/repos/brunomestanza/ignite-react/issues/${postId}`,
       )
 
+      console.log(response)
       setPostData(response.data)
     },
     [postId],
@@ -44,13 +47,16 @@ export function Publication() {
     <div className="max-w-[864px] flex align-center flex-col mx-auto">
       <div className="bg-brand-baseProfile flex flex-col p-8 rounded-[10px] mt-[-88px] mb-10">
         <header className="flex justify-between text-brand-blue">
-          <span className="flex gap-2 items-center font-bold text-sm">
+          <Link to="/" className="flex gap-2 items-center font-bold text-sm">
             <FaLessThan size={12} /> VOLTAR
-          </span>
-          <span className="flex gap-2 items-center font-bold text-sm">
+          </Link>
+          <a
+            href={postData.html_url}
+            className="flex gap-2 items-center font-bold text-sm"
+          >
             VER NO GITHUB
             <FaExternalLinkAlt size={12} />
-          </span>
+          </a>
         </header>
         <h1 className="font-bold text-2xl text-brand-baseTitle mb-2 mt-5">
           {postData.title}
@@ -61,7 +67,12 @@ export function Publication() {
           </span>
           <span className="flex gap-2 items-center text-brand-baseSpan">
             <FaCalendarDay size={18} className="text-brand-baseLabel" />
-            {postData.created_at}
+            {postData.created_at !== undefined
+              ? formatDistanceToNow(new Date(postData.created_at), {
+                  addSuffix: true,
+                  locale: ptBr,
+                })
+              : ''}
           </span>
           <span className="flex gap-2 items-center text-brand-baseSpan">
             <FaComment size={18} className="text-brand-baseLabel" />
@@ -69,7 +80,7 @@ export function Publication() {
           </span>
         </footer>
       </div>
-      <main>
+      <main className="mb-10">
         <MarkdownContent content={postData.body} />
       </main>
     </div>
