31bcd424673acf77b0df14d216c56235ddd64cd2
Bruno Mestanza <brunomonteiromestanza@gmail.com>
2022-11-22 23:35:18 -0300

    Bugfix

--- ignite-shop/src/pages/_app.tsx
+++ ignite-shop/src/pages/_app.tsx
@@ -1,18 +1,40 @@
 import type { AppProps } from 'next/app'
-import { globalStyles } from '../styles/global'
-import logoImg from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/app'
 import Image from 'next/image'
+import Link from 'next/link'
+import { CartProvider } from 'use-shopping-cart'
+import { Handbag } from 'phosphor-react'
+import { globalStyles } from '../styles/global'
+import { CartContainer, Container, Header } from '../styles/pages/app'
+import logoImg from '../assets/logo.svg'
 
 globalStyles() // Colocado aqui porque se ele fica dentro da função app, ele vai acabar sendo executado novamente dentro de cada uma das páginas
 
+const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+const cancelUrl = `${process.env.NEXT_URL}/`
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <CartProvider
+      mode="payment"
+      successUrl={successUrl}
+      cancelUrl={cancelUrl}
+      stripe={process.env.STRIPE_SECRET_KEY as string}
+      cartMode="client-only"
+      currency="BRL"
+      shouldPersist
+    >
+      <Container>
+        <Header>
+          <Link href="/">
+            <Image src={logoImg} alt="" />
+          </Link>
+          <CartContainer>
+            <span>1</span>
+            <Handbag size={48} weight="bold" />
+          </CartContainer>
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   )
 }
--- ignite-shop/src/styles/pages/app.ts
+++ ignite-shop/src/styles/pages/app.ts
@@ -13,4 +13,33 @@ export const Header = styled('header', {
   maxWidth: 1180,
   width: '100%',
   margin: '0 auto',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
+
+export const CartContainer = styled('div', {
+  position: 'relative',
+
+  span: {
+    position: 'absolute',
+    right: -15,
+    top: -20,
+    backgroundColor: '$green500',
+    borderRadius: 1000,
+    padding: 14,
+    height: 20,
+    width: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '5px solid black',
+  },
+
+  svg: {
+    padding: 12,
+    backgroundColor: '$gray800',
+    borderRadius: 6,
+    color: '$gray300',
+  },
 })
