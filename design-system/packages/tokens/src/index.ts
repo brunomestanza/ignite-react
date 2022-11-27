05cc024ce792cb75f119a2db568816cf9820a7bb
Bruno Mestanza <brunomonteiromestanza@gmail.com>
2022-11-27 01:31:06 -0300

    Bugfix

---
+++ design-system/packages/tokens/src/index.ts
@@ -0,0 +1,10 @@
+// Usamos o index pra que não seja necessário importar os tokens de forma extensa
+// Ao invés de import { colors } from '@ignite-ui/tokens/colors', podemos usar '@ignite-ui/tokens'
+
+export * from './colors'
+export * from './space'
+export * from './radii'
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
